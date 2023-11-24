78b1dff61e053c3e06827aac6a5cac71354e28e8
shamim_personal <shamim.hossain@selise.ch>
2023-11-24 21:20:53 +0600

    signup and signin from  with strapi validation done

--- front-end/src/pages/SignInFrom.jsx
+++ front-end/src/pages/SignInFrom.jsx
@@ -1,15 +1,89 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+// SignInForm.js
 
+import React, { useState } from 'react';
 
-const SignInFrom = () => {
+const SignInForm = () => {
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    identifier: '',
+    password: '',
+  });
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      setLoading(true);
+
+      const response = await fetch('http://localhost:1337/api/auth/local', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      console.log(response.status);  // Log the HTTP status code
+
+      if (response.ok) {
+        console.log('Login successful!');
+
+        navigate('/');
+        // Handle success, such as updating user state or redirecting
+      } else {
+        const data = await response.json();
+        console.error('Login failed:', data);
+        // Handle failure, such as displaying an error message to the user
+      }
+    } catch (error) {
+      console.error('Error submitting login:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <>
-      <div className="text-black">
+      <div className="text-black mb-4">
         <div className="font-semibold mb-4">SignInFrom</div>
         <Link to='/' className="p-3 bg-red-400 rounded font-semibold">Home</Link>
+      </div>
+      <div>
+        <h2>Sign In</h2>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Email or Username:
+            <input
+              type="text"
+              name="identifier"
+              value={formData.identifier}
+              onChange={handleChange}
+            />
+          </label>
+          <br />
+          <label>
+            Password:
+            <input
+              type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+            />
+          </label>
+          <br />
+          <button type="submit" disabled={loading}>
+            {loading ? 'Logging in...' : 'Log In'}
+          </button>
+        </form>
       </div>
     </>
   );
 };
 
-export default SignInFrom;
+export default SignInForm;
--- front-end/src/pages/SignUpFrom.jsx
+++ front-end/src/pages/SignUpFrom.jsx
@@ -1,15 +1,88 @@
+import { useState } from 'react';
 import { Link } from "react-router-dom";
 
+// src/components/RegistrationForm.js
+
+import axios from "axios";
 
 const SignUpFrom = () => {
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    password: '',
+  });
+  const [loading, setLoading] = useState(false);
+
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      setLoading(true);
+
+      const response = await fetch('http://localhost:1337/api/auth/local/register', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      console.log(response.status);  // Log the HTTP status code
+
+      if (response.ok) {
+        console.log('Registration successful!');
+        // Handle success, such as redirecting the user to a login page
+      } else {
+        const data = await response.json();
+        console.log(data);
+        console.error('Registration failed:', data);
+        // Handle failure, such as displaying an error message to the user
+      }
+    } catch (error) {
+      console.error('Error submitting registration:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+    console.log(`${name}: ${value}`);
+  };
+
+
   return (
     <>
       <div className="text-black">
-        <div className="font-semibold mb-4">SignUpFrom</div>
+        <div className="font-semibold mb-4">SignUpForm</div>
         <Link to='/' className="p-3 bg-red-400 rounded font-semibold">Home</Link>
       </div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username:
+          <input type="text" name="username" value={formData.username} onChange={handleChange} />
+        </label>
+        <br />
+        <label>
+          Email:
+          <input type="email" name="email" value={formData.email} onChange={handleChange} />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input type="password" name="password" value={formData.password} onChange={handleChange} />
+        </label>
+        <br />
+        <button type="submit" disabled={loading}>
+          {loading ? 'Registering...' : 'Register'}
+        </button>
+      </form>
     </>
   );
 };
+
 
 export default SignUpFrom;
